9e5f49a764e41ccd96f877c073c3bc511b84daae
Hen Simkin <hensi1@ac.sce.ac.il>
2023-05-28 20:51:56 +0300

    location of grades

--- index.js
+++ index.js
@@ -20,6 +20,45 @@ async function connect() {
   
 connect();
 
+
+
+const gradesSchema = new mongoose.Schema({
+  fullName: String,
+  grade1: Number,
+  grade2: Number,
+  grade3: Number,
+});
+
+// Define the Mongoose model
+const Grades = mongoose.model('Grades', gradesSchema);
+
+
+
+app.post('/grades', async (req, res) => {
+  const { fullName, grade1, grade2, grade3 } = req.body;
+  const averageGrade = (parseInt(grade1) + parseInt(grade2) + parseInt(grade3)) / 3;
+
+  // Create a new instance of the Grades model
+  const grades = new Grades({
+    fullName: fullName,
+    grade1: grade1,
+    grade2: grade2,
+    grade3: grade3,
+  });
+
+  try {
+    await grades.save();
+    console.log('Grades saved successfully1');
+    res.sendStatus(200);
+  } catch (err) {
+    console.error('Error saving grades to MongoDB2:', err);
+    res.sendStatus(500);
+  }
+});
+
+
+
+
 const port=process.env.PORT || 3000
 
 app.listen(port,()=>{
