a42ae2ee33bff41c96c340ac927ebc5c27ff7cb4
Hen Simkin <hensi1@ac.sce.ac.il>
2023-05-29 12:21:29 +0300

    tests

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-const mongoose =require('mongoose');
-const app = require('./server')
+const mongoose = require('mongoose');
+const app = require('./server');
 
-const uri ='mongodb+srv://Hensim977:06061997H@devops.mlysp3s.mongodb.net/?retryWrites=true&w=majority'
-
+const uri = 'mongodb+srv://Hensim977:06061997H@devops.mlysp3s.mongodb.net/?retryWrites=true&w=majority';
 
 async function connect() {
-    try {
-      await mongoose.connect(uri);
-      
-      if (process.env.NODE_ENV === 'test') {
-        console.log("Connected to URI"); 
-      } else {
-        console.error("Connected to URI"); 
-      }
-    } catch (error) {
-      console.error("error");
+  try {
+    await mongoose.connect(uri);
+
+    if (process.env.NODE_ENV === 'test') {
+      console.log('Connected to URI');
+    } else {
+      console.error('Connected to URI');
     }
+  } catch (error) {
+    console.error('error');
   }
-  
+}
+
 connect();
-
-
 
 const gradesSchema = new mongoose.Schema({
   fullName: String,
@@ -29,16 +26,12 @@ const gradesSchema = new mongoose.Schema({
   grade3: Number,
 });
 
-// Define the Mongoose model
 const Grades = mongoose.model('Grades', gradesSchema);
-
-
 
 app.post('/grades', async (req, res) => {
   const { fullName, grade1, grade2, grade3 } = req.body;
   const averageGrade = (parseInt(grade1) + parseInt(grade2) + parseInt(grade3)) / 3;
 
-  // Create a new instance of the Grades model
   const grades = new Grades({
     fullName: fullName,
     grade1: grade1,
@@ -56,11 +49,12 @@ app.post('/grades', async (req, res) => {
   }
 });
 
+if (require.main === module) {
+  const port = process.env.PORT || 3000;
 
+  app.listen(port, () => {
+    console.log('server started');
+  });
+}
 
-
-const port=process.env.PORT || 3000
-
-app.listen(port,()=>{
-    console.log("server started")
-})
+module.exports = app;
--- tests/test1.test.js
+++ tests/test1.test.js
@@ -1,15 +1,30 @@
-const request = require('supertest')
-const app = require('../server')
+const request = require('supertest');
+const app = require('../server');
+const app2 = require('../index');
 
+describe('Grade Form Input Tests', () => {
+  let server;
 
-describe("test suite 1:",()=>{
-    test("test 1: ",async ()=>{
-        const res=await request(app).get('/')
-        expect(res.statusCode).toEqual(200)
-    })
+  beforeAll(() => {
+    server = app.listen(3000);
+  });
 
-    test("test 2: ",async ()=>{
-        const res=await request(app).get('/1234')
-        expect(res.statusCode).toEqual(404)
-    })
-})
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  test('Submitting valid input should return 200', async () => {
+    const validInput = {
+      fullName: 'John Doe',
+      grade1: 85,
+      grade2: 92,
+      grade3: 78
+    };
+
+    const res = await request(app)
+      .post('/grades')
+      .send(validInput);
+
+    expect(res.statusCode).toEqual(200);
+  });
+});
